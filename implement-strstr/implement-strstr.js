291fb58c0694fd6eca6c53ac956d94117d8a3a6a
Sobolevsky Dmitry <15331773+SobolevskyDmitry@users.noreply.github.com>
2021-07-27 13:59:40 +0200

    Time: 88 ms (43.20%), Space: 41.8 MB (5.20%) - LeetHub

--- implement-strstr/implement-strstr.js
+++ implement-strstr/implement-strstr.js
@@ -3,26 +3,61 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
-    if(!haystack && !needle || haystack && !needle || haystack === needle) {
+var strStr = function(text, word) {
+    if (!text && !word) {
         return 0;
     }
     
-
-    const needleLength = needle.length;
-    let test = '';
-    
-    for(let i = 0; i < haystack.length; i++) {
-        test+= haystack[i];
-        
-        if(test.slice(-needleLength) === needle) {
-            return i - needleLength + 1;
-        }
+    if (text && !word) {
+        return 0;
     }
     
-    
+    const table = buildTable(word);
+    let textIndex = 0;
+    let wordIndex = 0;
+
+    while(textIndex < text.length) {
+        if(text[textIndex] === word[wordIndex]) {
+
+            if(wordIndex === word.length - 1){
+                return textIndex - wordIndex;
+            }
+
+            textIndex++;
+            wordIndex++;
+
+        } else {
+            if(wordIndex === 0) {
+                textIndex++;
+            } else {
+                wordIndex = table[wordIndex - 1]
+            }
+        }
+    }
+
     return -1;
 };
 
-//abc
-//  c
+function buildTable(pattern) {
+    const table = [0];
+    
+    let prefixIndex = 0;
+    let suffixIndex = 1;
+    
+    while(suffixIndex < pattern.length) {
+        if(pattern[prefixIndex] === pattern[suffixIndex]){
+            table[suffixIndex] = prefixIndex + 1;
+            prefixIndex++;
+            suffixIndex++;
+        } else {
+            if(prefixIndex === 0) {
+                table[suffixIndex] = 0;
+                suffixIndex++;
+            } else {
+                prefixIndex = table[prefixIndex - 1];
+            }
+        }
+    }
+
+    return table;
+}
