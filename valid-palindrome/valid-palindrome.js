ee847d2442a52df9faa1b52bc41837e2cdbefeaa
Sobolevsky Dmitry <15331773+SobolevskyDmitry@users.noreply.github.com>
2021-08-02 17:40:55 +0200

    Time: 92 ms (57.51%), Space: 40.6 MB (94.52%) - LeetHub

---
+++ valid-palindrome/valid-palindrome.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+         v                 v
+ "A man, a plan, a canal: Panama"
+ */
+var isPalindrome = function(s) {
+   s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase(); 
+   let left = 0;
+   let right = s.length - 1;
+    
+    
+    while (left < right) {
+        if (s[left] !== s[right]){
+           return false;
+        }
+        
+        left++;
+        right--;
+    }
+    
+    return true;
+    
+};
