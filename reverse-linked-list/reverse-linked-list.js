002a0fe93dffe08f40b96f26ba02870fc9f359b5
Sobolevsky Dmitry <15331773+SobolevskyDmitry@users.noreply.github.com>
2021-08-08 21:55:29 +0200

    Update reverse-linked-list.js

--- reverse-linked-list/reverse-linked-list.js
+++ reverse-linked-list/reverse-linked-list.js
@@ -9,30 +9,19 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
 var reverseList = function(head) {
-    if (!head) {
-        return null
+    // End of the reversed linked list set to null
+    let prev = null;
+    
+    // Traverse through the given linked list
+    while (head) {
+        const temp = head.next;     // References the next Node of given linked list
+        head.next = prev;       // head.next point to previous Node, instead of the next Node of the given linked list
+        prev = head;        // Move the prev Node pointer up to head
+        head = temp;        // Move the head up to next Node of the given linked list
     }
     
-    const arr = [];
-    
-    while(head) {
-        const next = head.next;
-        
-        head.next = null;
-        arr.push(head);  
-        
-        head = next;
-    }
-    
-    const newHead = arr[arr.length - 1];
-    
-    let temp = newHead;
-    
-    for (let i = arr.length - 2; i >=0; i--) {
-        temp.next = arr[i];
-        temp = temp.next;
-    }
-    
-    return newHead;
+    // Prev is the reversed linked list
+    return prev;
 };
