9f04c2f2d47108496c856930ec2f0a788bce1ad5
Sobolevsky Dmitry <15331773+SobolevskyDmitry@users.noreply.github.com>
2021-08-09 14:57:57 +0200

    Time: 88 ms (77.95%), Space: 40.5 MB (83.61%) - LeetHub

---
+++ valid-anagram/valid-anagram.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+    
+    const map = {};
+    
+    for(let i = 0; i < s.length; i++) {
+        map[s[i]] = map[s[i]] ? map[s[i]] + 1 : 1;
+    }
+    
+    for (let i = 0; i < t.length; i++) {
+        if (!map[t[i]]) {
+            return false;
+        }
+        
+        map[t[i]]--;
+    }
+    
+    return true;
+};
