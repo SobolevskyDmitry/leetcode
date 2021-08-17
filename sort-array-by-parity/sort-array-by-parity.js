726f049f1bbcda50fcad5a48e34b2f71819eb5d8
Sobolevsky Dmitry <15331773+SobolevskyDmitry@users.noreply.github.com>
2021-08-17 17:34:40 +0200

    Time: 100 ms (25.17%), Space: 42.1 MB (15.70%) - LeetHub

---
+++ sort-array-by-parity/sort-array-by-parity.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+    const res = [];
+    
+    for(let num of nums) {
+        if(num % 2 === 0) {
+            res.unshift(num);
+        } else {
+            res.push(num);
+        }
+    }   
+    return res;
+};
