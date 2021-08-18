03af5c6e2f2c639e879d26d05368e7318a7af8d4
Sobolevsky Dmitry <15331773+SobolevskyDmitry@users.noreply.github.com>
2021-08-18 16:48:44 +0200

    Time: 88 ms (81.28%), Space: 41.2 MB (62.78%) - LeetHub

--- sort-array-by-parity/sort-array-by-parity.js
+++ sort-array-by-parity/sort-array-by-parity.js
@@ -3,14 +3,38 @@
  * @return {number[]}
  */
 var sortArrayByParity = function(nums) {
-    const res = [];
+    let right = nums.length - 1;
+    let left = 0;
     
-    for(let num of nums) {
-        if(num % 2 === 0) {
-            res.unshift(num);
-        } else {
-            res.push(num);
+    
+    while(left < right) {
+        if(nums[left] % 2 && nums[right] % 2 === 0) {
+            
+            swap(nums,left, right);
+            right--;    
+        }   
+       
+        if(nums[left] % 2 && nums[right] % 2) {
+            while(left < right && nums[right] % 2) {
+                right--;
+            }
+            
+            if(nums[right] % 2 === 0) {
+                swap(nums, left, right);
+                right--;
+            }
         }
-    }   
-    return res;
+        
+        left++;
+    }
+    
+   
+    return nums;
 };
+
+
+function swap(arr, l, r) {
+    let temp = arr[l];
+    arr[l] = arr[r];
+    arr[r] = temp;
+}
