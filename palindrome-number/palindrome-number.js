eb82bbcbd5a5ccb92f69e8f0eea14859dd66c6a5
Sobolevsky Dmitry <15331773+SobolevskyDmitry@users.noreply.github.com>
2021-08-30 17:48:40 +0200

    Time: 292 ms (14.95%), Space: 48.8 MB (17.54%) - LeetHub

---
+++ palindrome-number/palindrome-number.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    const str = x.toString();
+    
+    let l = 0, r = str.length - 1;
+    
+    while(l <=r) {
+        if(str[l] !== str[r]) {
+            return false;
+        }
+        
+        l++;
+        r--;
+    }
+    
+    return true;
+};
