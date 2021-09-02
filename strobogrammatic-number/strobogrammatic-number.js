677c523a8f020b16699492f4b070bd5504c428de
Sobolevsky Dmitry <15331773+SobolevskyDmitry@users.noreply.github.com>
2021-09-02 17:28:49 +0200

    Time: 69 ms (66.67%), Space: 38.6 MB (63.64%) - LeetHub

--- strobogrammatic-number/strobogrammatic-number.js
+++ strobogrammatic-number/strobogrammatic-number.js
@@ -6,11 +6,17 @@ var isStrobogrammatic = function(num) {
     const rotatedMap = {0: 0, 1: 1, 2: 0, 3: 0, 4: 0, 5: 0, 6: 9, 7: 0, 8: 8, 9:6};
     
     let strNumber = num.toString();
-    let newNumber = '';
     
-    for(let i = strNumber.length - 1; i >=0; i--) {
-        newNumber +=rotatedMap[strNumber[i]]
+    let i = 0, j = strNumber.length - 1;
+    
+    while(i <= j) {
+        if(+strNumber[i] !== +rotatedMap[strNumber[j]]) {
+            return false;
+        }
+        
+        i++;
+        j--;
     }
     
-    return +strNumber === +newNumber;
+    return true
 };
